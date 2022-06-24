bcb0ec0cc2994c13b1318bae70d107e851b242d9
didiermun <mugishaisaac2001@gmail.com>
2022-06-24 10:08:21 +0200

    feat: signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
 import SingUp from "./views/SingUp";
 import "./index.css";
+import Logo from "./images/Vector.png";
+import OngoingElections from "./components/OngoingElections";
 
 function App() {
   return (
-    <div className="w-[115rem] m-1 max-h-fit bg-[#0089ED] flex space-x-3">
-      <div className="w-[40%] ml-10">
+    <div className="w-[115rem] m-1 overflow-y-hidden">
+    <div className="bg-[#0089ED] h-[615px] flex space-x-3">
+      <div className="w-[50%] ml-10">
         <p className="px-8 py-5 ml-10 text-[48px] text-white font-bold">LOGO</p>
-        <div className="flex flex-row gap-2 mt-12">
+        <div className="flex flex-row gap-8 mt-[8rem]">
           <div className="ml-10 px-8 py-4 w-[60%]">
-              <p className="text-[40px]  font-bold text-white">Sign Up to</p>
-              <p className="text-[26px] font-[400] text-[#FFFFFF] py-1">vote for future leaders</p>
-              <p className="py-8 text-[#FFFFFF] text-[20px] font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, cupiditate. Eum deleniti accusamus fugit voluptatum enim eius soluta quo hic ab in repellat, impedit veritatis vero commodi obcaecati minus iusto.</p>
+            <p className="text-[40px]  font-bold text-white">Sign Up to</p>
+            <p className="text-[26px] font-[400] text-[#FFFFFF] py-1">
+              vote for future leaders
+            </p>
+            <p className="py-8 text-[#FFFFFF] text-[20px] font-[300]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              cupiditate. Eum deleniti accusamus fugit voluptatum enim eius
+              soluta quo hic ab in repellat, impedit veritatis vero commodi
+              obcaecati minus iusto.
+            </p>
           </div>
-          <div>
-            <img src="./images/Vector.png" width="300px" height="300px" alt="logo" />
+          <div className="-mt-[4rem] ml-10">
+            <img src={Logo} width="120px" height="120px" alt="logo" />
           </div>
-          </div>
+        </div>
       </div>
+      <div></div>
       <div>
-        <SingUp />
+        <SingUp /> 
       </div>
+    </div>
+    <div className="m-4">
+    <OngoingElections />
+    </div>
     </div>
   );
 }
--- src/views/SingUp.jsx
+++ src/views/SingUp.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../index.css'
+import React from "react";
+import "../index.css";
 
 function SingUp() {
   return (
-    <div>SingUp</div>
-  )
+    <div className="bg-white m-2 w-[657px] h-[859px] rounded-[50px] mt-[5rem] text-center drop-shadow-[0px 2px 1px rgba(218, 81, 230, 0.25)]">
+      SingUp
+    </div>
+  );
 }
 
-export default SingUp
+export default SingUp;
