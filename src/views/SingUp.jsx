2d72c5a0098a1963d046ac0fc2e3f5b7acf35a1f
didiermun <mugishaisaac2001@gmail.com>
2022-06-24 12:32:07 +0200

    feat: signup design finished

--- src/views/SingUp.jsx
+++ src/views/SingUp.jsx
@@ -4,7 +4,7 @@ import Button from "../components/Button";
 
 function SingUp() {
   return (
-    <div className="bg-[#FFFFFF] m-2 w-[657px] h-[819px] rounded-[50px] mt-[5rem] drop-shadow-[0px 2px 1px rgba(218, 81, 230, 0.25)]">
+    <div className="bg-[#FFFFFF] m-2 w-[657px] h-[819px] rounded-[50px] mt-[5rem] shadow-lg">
       <div className="flex flex-row space-x-[10rem]">
         <div className=" px-10 flex flex-row gap-1 py-[25px]">
           <p className="text-[21px] font-[400]">Welcome to</p>
@@ -26,13 +26,31 @@ function SingUp() {
           name="nid"
           id="nid"
           placeholder="National Id"
-          className="w-[520px] h-[55px] border-2 rounded-[16px] focus:outline-none ml-8 mt-[50px] font-[500] text-[21px] px-[4rem]"
+          className="w-[520px] h-[55px] border-2 rounded-[16px] focus:outline-none ml-8 mt-[50px] font-[500] text-[21px] px-[4rem] focus:border-[#0089ED]"
         />
         <div className="flex flex-row gap-8">
-          <input type="text" name="names" id="name" placeholder="Your Names" className="w-[230px] h-[60px] border-2 mt-[40px] ml-8 rounded-[10px] text-center" />
-          <input type="text" name="email" id="email" placeholder="Email Address" className="w-[230px] h-[60px] border-2 mt-[40px] ml-8 rounded-[10px] text-center" />
+          <input
+            type="text"
+            name="names"
+            id="name"
+            placeholder="Your Names"
+            className="w-[230px] h-[60px] border-2 mt-[40px] ml-8 rounded-[10px] text-center focus:outline-none focus:border-[#0089ED]"
+          />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            className="w-[230px] h-[60px] border-2 mt-[40px] ml-8 rounded-[10px] text-center focus:outline-none focus:border-[#0089ED]"
+          />
         </div>
-        <input type="password" name="password" id="password" placeholder="password" className="w-[520px] h-[55px] border-2 rounded-[16px] focus:outline-none ml-8 mt-[50px] font-[500] text-[21px] px-[4rem]" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          className="w-[520px] h-[55px] border-2 rounded-[16px] focus:outline-none ml-8 mt-[50px] font-[500] text-[21px] px-[4rem] focus:border-[#0089ED]"
+        />
         <Button type="Signup" />
       </form>
     </div>
