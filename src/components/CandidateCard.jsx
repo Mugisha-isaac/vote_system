486d567ef6f866e78a947d13b58acd0a9839de11
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-01 15:56:02 -0400

    [style] styling vote button

--- src/components/CandidateCard.jsx
+++ src/components/CandidateCard.jsx
@@ -17,7 +17,7 @@ const CandidateCard = () => {
             <img src={candidate.imgUrl} alt="candidate profile" className=" object-cover  rounded-full w-28 h-28 mx-auto absolute top-[9.72%] left-[18.75%] right-[26.47%] bottom-[47.5%]" />
              <p className="absolute top-[55.92%] left-[17.13%] right-[11.48%] bottom-[38.04%] text-[20px] whitespace-pre ">{candidate.names}</p>
              <p className="absolute top-[65.24%] right-[28.7%] left-[30.74%] bottom-[28.72%] py-[24px] text-[#636262] font-[400]">{candidate.votes} votes</p>
-             <button className="absolute top-[77.83%] left-[16.53%] right-[17.33%] bottom-[8.06%] bg-[#0089ED] mt-4 text-white font-bold">Vote</button>
+             <button className="absolute top-[77.83%] left-[16.53%] right-[17.33%] bottom-[8.06%] bg-[#0089ED] mt-4 text-white font-bold focus:outline-none active:bg-blue-900 focus:ring focus:ring-blue-300">Vote</button>
         </div>
       ))}
     </div>
